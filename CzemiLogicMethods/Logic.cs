bfd1c58f3ad56374a281693a1097d5c7a63cb9ec
Ssemakula Mukiibi <Ssemakula@users.noreply.github.com>
2025-02-24 21:05:02 +0300

    Add project files.

---
+++ CzemiLogicMethods/Logic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Czemi.LogicMethods
+{
+    public static class Logic
+    {
+        public static bool IsTrue(this int value) => value != 0;
+
+        public static bool IsTrue(this uint value) => value != 0U;
+
+        public static bool IsTrue(this long value) => value != 0L;
+
+        public static bool IsTrue(this ulong value) => value != 0UL;
+
+        public static bool IsTrue(this short value) => value != 0;
+
+        public static bool IsTrue(this ushort value) => value != 0;
+
+        public static bool IsTrue(this BigInteger value) => value != 0;
+
+        public static bool IsTrue(this byte value) => value != 0;
+
+        public static bool IsTrue(this sbyte value) => value != 0;
+
+        public static bool IsTrue(this float value) => value != 0f;
+
+        public static bool IsTrue(this double value) => value != 0d;
+
+        public static bool IsTrue(this decimal value) => value != 0m;
+
+        public static bool IsTrue(this Enum value) => Convert.ToInt64(value) != 0;
+
+        public static bool IsTrue(this string value) => !string.IsNullOrEmpty(value);
+
+        public static bool IsTrue(this object value) => value != null;
+    }
+}
